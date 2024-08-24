56569b91b7d9a22e521aa8d0b9d08b825bca3823
Tobias Schiffelhumer <to.schiffelhumer@gmail.com>
2024-08-24 22:57:57 +0200

    Type Indicator

--- drivelog/lib/widgets/mileage_table.dart
+++ drivelog/lib/widgets/mileage_table.dart
@@ -25,12 +25,17 @@ class _MileageTableState extends State<MileageTable> {
       delimiter = x;
     }));
   }
+  
+  double calculateWidth(BuildContext context){
+    return MediaQuery.sizeOf(context).width / 10 + 8 * MediaQuery.of(context).devicePixelRatio;
+  }
 
   DataRow mapEvent(Event event,  BuildContext context){
     return DataRow(cells: [
-      DataCell(SizedBox(width: MediaQuery.sizeOf(context).width / 5,child: Center(child: Text(DateFormat('dd.MM').format(event.dateAdded),)))),
-      DataCell(SizedBox(width: MediaQuery.sizeOf(context).width / 5,child: Center(child: Text("${event.distance??"-- "}km".replaceAll(".", delimiter))))),
-      DataCell(SizedBox(width: MediaQuery.sizeOf(context).width / 5,child: Center(child: Text("${event.fuelConsumption??"-- "}l".replaceAll(".", delimiter))))),
+      DataCell(SizedBox(width: calculateWidth(context) - 21 * MediaQuery.of(context).devicePixelRatio, child: const Center(child:  Icon(Icons.directions_car)))),
+      DataCell(SizedBox(width: calculateWidth(context), child: Center(child: Text(DateFormat('dd.MM').format(event.dateAdded),)))),
+      DataCell(SizedBox(width: calculateWidth(context), child: Center(child: Text("${event.distance??"-- "}km".replaceAll(".", delimiter))))),
+      DataCell(SizedBox(width: calculateWidth(context), child: Center(child: Text("${event.fuelConsumption??"-- "}l".replaceAll(".", delimiter))))),
     ]);
   }
 
@@ -40,9 +45,10 @@ class _MileageTableState extends State<MileageTable> {
       width: MediaQuery.of(context).size.width,
       child: DataTable(
         columns: [
-          DataColumn(label: SizedBox(width: MediaQuery.sizeOf(context).width / 5, child: const Center(child: Icon(Icons.calendar_month,)))),
-          DataColumn(label: SizedBox(width: MediaQuery.sizeOf(context).width / 5, child: const Center(child: Icon(Icons.speed)))),
-          DataColumn(label: SizedBox(width: MediaQuery.sizeOf(context).width / 5, child: const Center(child: Icon(Icons.local_gas_station)))),
+          DataColumn(label: SizedBox(width: calculateWidth(context) - 21 * MediaQuery.of(context).devicePixelRatio, child: null)),
+          DataColumn(label: SizedBox(width: calculateWidth(context), child: const Center(child: Icon(Icons.calendar_month,)))),
+          DataColumn(label: SizedBox(width: calculateWidth(context), child: const Center(child: Icon(Icons.speed)))),
+          DataColumn(label: SizedBox(width: calculateWidth(context), child: const Center(child: Icon(Icons.local_gas_station)))),
         ],
         rows: widget.events.map((x) => mapEvent(x, context)).toList(growable: false),
       ),
